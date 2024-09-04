5eccfedc39e11cfcbdc0e0771690be173a8a65df
rreiniks <rikucis5@gmail.com>
2024-09-04 15:11:00 +0300

    Exercise: create a configurable component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { RouterOutlet } from '@angular/router';
 import { UserComponent } from './user/user.component';
 
 import { DUMMY_USERS } from './user/dummy-users';
+import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, UserComponent],
+  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  @Output() select = new EventEmitter<string>();
   users = DUMMY_USERS;
+  selectedName = '';
 
   onSelectUser(id: string) {
-    console.log('Selected user with id ' + id);
+    const user = DUMMY_USERS.find(user => user.id === id);
+    this.selectedName = user!.name;
   }
 }
---
+++ src/app/tasks/tasks.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+let name = '';
+
+@Component({
+  selector: 'app-tasks',
+  standalone: true,
+  imports: [],
+  templateUrl: './tasks.component.html',
+  styleUrl: './tasks.component.scss'
+})
+export class TasksComponent {
+  @Input({required: true}) selectedName!: string;
+}
