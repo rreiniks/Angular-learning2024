e71e949e57b8c8c0037793a6a280ec8369287615
rreiniks <rikucis5@gmail.com>
2024-09-11 10:45:49 +0300

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RectComponent } from './rect/rect.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RectComponent, FormsModule],
 })
 export class AppComponent {
-  title = 'first-angular-app';
+  rectSize = {
+    width: '100',
+    height: '100',
+  };
 }
