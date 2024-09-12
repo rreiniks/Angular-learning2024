efb08505a7622a47c7f3175fc208d9755ee59e0f
rreiniks <rikucis5@gmail.com>
2024-09-12 10:59:06 +0300

    Executing code using directive

--- src/app/safe-link.directive.ts
+++ src/app/safe-link.directive.ts
@@ -3,10 +3,21 @@ import { Directive } from '@angular/core';
 @Directive({
     selector: 'a[appSafeLink]',
     standalone: true,
-
+    host: {
+        '(click)': 'onConfirmLeavePage($event)'
+    }
 })
 export class SafeLinkDirective {
     constructor(){
-        alert('It is active');
+    }
+
+    onConfirmLeavePage(event: MouseEvent){
+        const wantsToLeave = window.confirm('Do you want to leave the app?');
+
+        if(wantsToLeave){
+            return;
+        }
+
+        event?.preventDefault();
     }
 }
