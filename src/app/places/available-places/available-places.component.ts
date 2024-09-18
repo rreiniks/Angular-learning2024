99f1cc6985d1b6c5ae760e22c0586cf3ea9ce43d
rreiniks <rikucis5@gmail.com>
2024-09-18 11:46:38 +0300

    Showing a loading fallback

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -15,10 +15,12 @@ import { map } from 'rxjs';
 })
 export class AvailablePlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
+  isFetching = signal(false);
   private httpClient = inject(HttpClient);
   private destroyRef = inject(DestroyRef);
 
   ngOnInit() {
+    this.isFetching.set(true);
     const subscription = this.httpClient
       .get<{ places: Place[] }>('http://localhost:3000/places')
       .pipe(
@@ -28,6 +30,13 @@ export class AvailablePlacesComponent implements OnInit {
         next: (places) => {
           this.places.set(places);
         },
+        complete: () => {
+          this.isFetching.set(false);
+          return;
+        },
+        error: (err) => {
+          
+        }
       });
 
     this.destroyRef.onDestroy(() => {
