41baea0fb10c235eadb4aba6b45c0c082c9c7234
rreiniks <rikucis5@gmail.com>
2024-09-18 11:55:32 +0300

    Handling errors

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -4,7 +4,7 @@ import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs';
+import { catchError, map, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-available-places',
@@ -16,6 +16,7 @@ import { map } from 'rxjs';
 export class AvailablePlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
   isFetching = signal(false);
+  error = signal('');
   private httpClient = inject(HttpClient);
   private destroyRef = inject(DestroyRef);
 
@@ -24,7 +25,16 @@ export class AvailablePlacesComponent implements OnInit {
     const subscription = this.httpClient
       .get<{ places: Place[] }>('http://localhost:3000/places')
       .pipe(
-        map((resData) => resData.places)
+        map((resData) => resData.places),
+        catchError((error) => {
+          console.log(error);
+          return throwError(
+            () =>
+              new Error(
+                'Something went wrong fetching the available places. Please try again later!'
+              )
+          );
+        })
       )
       .subscribe({
         next: (places) => {
@@ -34,9 +44,9 @@ export class AvailablePlacesComponent implements OnInit {
           this.isFetching.set(false);
           return;
         },
-        error: (err) => {
-          
-        }
+        error: (error: Error) => {
+          this.error.set(error.message);
+        },
       });
 
     this.destroyRef.onDestroy(() => {
