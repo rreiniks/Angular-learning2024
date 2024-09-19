8b00eaefe9355376f125117e493cfb8c670a88df
rreiniks <rikucis5@gmail.com>
2024-09-19 12:13:59 +0300

    Adding exercise inputs

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,9 +1,32 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css',
 })
-export class SignupComponent {}
+export class SignupComponent {
+  form = new FormGroup({
+    email: new FormControl('', {
+      validators: [Validators.email, Validators.required],
+    }),
+    password: new FormControl('', {
+      validators: [
+        Validators.required,
+        Validators.minLength(6),
+      ],
+    }),
+  });
+
+  onSubmit(){
+    console.log(this.form);
+    this.onReset();
+  }
+
+  onReset(){
+    this.form.reset();
+  }
+}
