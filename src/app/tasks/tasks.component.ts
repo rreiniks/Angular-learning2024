2857987ca08d3e9ebd2b5d3d1ac4e5cef9939079
rreiniks <rikucis5@gmail.com>
2024-09-20 14:07:09 +0300

    Extracting query params via inputs

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -2,19 +2,28 @@ import { Component, computed, inject, input } from '@angular/core';
 
 import { TaskComponent } from './task/task.component';
 import { TasksService } from './tasks.service';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
-  imports: [TaskComponent, RouterLink ],
+  imports: [TaskComponent, RouterLink],
 })
 export class TasksComponent {
   userId = input.required<string>();
+  /* order = input<'asc' | 'desc'>(); */
+  order?: 'asc' | 'desc';
   private tasksService = inject(TasksService);
   userTasks = computed(() =>
     this.tasksService.allTasks().filter((task) => task.userId === this.userId())
   );
+  private activatedRoute = inject(ActivatedRoute);
+
+  ngOnInit(): void {
+    this.activatedRoute.queryParams.subscribe({
+      next: (params) => (this.order = params['order']),
+    });
+  }
 }
