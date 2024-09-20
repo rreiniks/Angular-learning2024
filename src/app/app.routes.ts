4823c51b09a2b8c1114e5913656d7f85fda10280
rreiniks <rikucis5@gmail.com>
2024-09-20 09:57:45 +0300

    Added another route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
-import { Routes } from "@angular/router";
-import { TasksComponent } from "./tasks/tasks.component";
+import { Routes } from '@angular/router';
+import { TasksComponent } from './tasks/tasks.component';
+import { NoTaskComponent } from './tasks/no-task/no-task.component';
 
 export const routes: Routes = [
-    {
-      path: 'tasks',
-      component: TasksComponent,
-    },
-  ];
+  {
+    path: '',
+    component: NoTaskComponent,
+  },
+  {
+    path: 'tasks',
+    component: TasksComponent,
+  },
+];
