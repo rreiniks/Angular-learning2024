dd325f83c66f87b01e0d27c523ed24538526a88e
rreiniks <rikucis5@gmail.com>
2024-09-20 14:56:34 +0300

    Controlling route resolver execution

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,16 +1,13 @@
 import {
   Component,
-  computed,
-  DestroyRef,
   inject,
   input,
-  runInInjectionContext,
-  signal,
 } from '@angular/core';
+import { ResolveFn, RouterLink } from '@angular/router';
 
 import { TaskComponent } from './task/task.component';
 import { TasksService } from './tasks.service';
-import { ActivatedRoute, RouterLink } from '@angular/router';
+import { Task } from './task/task.model';
 
 @Component({
   selector: 'app-tasks',
@@ -20,27 +17,28 @@ import { ActivatedRoute, RouterLink } from '@angular/router';
   imports: [TaskComponent, RouterLink],
 })
 export class TasksComponent {
+  userTasks = input.required<Task[]>();
   userId = input.required<string>();
-  /* order = input<'asc' | 'desc'>(); */
-  destroyRef = inject(DestroyRef);
-  order = signal<'asc' | 'desc'>('desc');
-  private tasksService = inject(TasksService);
-  userTasks = computed(() =>
-    this.tasksService.allTasks().filter((task) => task.userId === this.userId()).sort((a, b) => {
-      if (this.order() === 'desc'){
-        return a.id > b.id ? -1 : 1 
-      }else{
-        return a.id > b.id ? 1 : -1
-      }
-    })
-  );
-  private activatedRoute = inject(ActivatedRoute);
-
-  ngOnInit(): void {
-    const subscription = this.activatedRoute.queryParams.subscribe({
-      next: (params) => this.order.set(params['order']),
-    });
-
-    this.destroyRef.onDestroy(() => subscription.unsubscribe());
-  }
+  order = input<'asc' | 'desc' | undefined>();
 }
+
+export const resolveUserTasks: ResolveFn<Task[]> = (
+  activatedRouteSnapshot,
+  routerState
+) => {
+  const order = activatedRouteSnapshot.queryParams['order'];
+  const tasksService = inject(TasksService);
+  const tasks = tasksService
+    .allTasks()
+    .filter(
+      (task) => task.userId === activatedRouteSnapshot.paramMap.get('userId')
+    );
+
+  if (order && order === 'asc') {
+    tasks.sort((a, b) => (a.id > b.id ? 1 : -1));
+  } else {
+    tasks.sort((a, b) => (a.id > b.id ? -1 : 1));
+  }
+
+  return tasks.length ? tasks : [];
+};
