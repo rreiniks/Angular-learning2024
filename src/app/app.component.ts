7cd313e90ae0b9d5532d4b40f4195cdae4c1ab79
rreiniks <rikucis5@gmail.com>
2024-09-23 10:23:21 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { HeaderComponent } from './header/header.component';
+import { UsersComponent } from './users/users.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.css',
+  imports: [HeaderComponent, UsersComponent, RouterOutlet],
 })
-export class AppComponent {
-  title = 'first-angular-app';
-}
+export class AppComponent {}
