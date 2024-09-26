9a96fb2433b9524a5a086093f4f088844b04cce9
rreiniks <rikucis5@gmail.com>
2024-09-26 12:04:00 +0300

    Initial commit

--- README.md
+++
@@ -1,4 +0,0 @@
-# Learning Angular newest version 2024
-Repo for tasks relating to this angular course - https://www.udemy.com/course/the-complete-guide-to-angular-2/learn/lecture/43788522#overview
-
-For refreshing/improving my angular knowledge
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+import { CounterOutputComponent } from './counter-output/counter-output.component';
+import { CounterControlsComponent } from './counter-controls/counter-controls.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.css'],
+  standalone: true,
+  imports: [CounterOutputComponent, CounterControlsComponent],
 })
-export class AppComponent {
-  title = 'first-angular-app';
-}
+export class AppComponent {}
